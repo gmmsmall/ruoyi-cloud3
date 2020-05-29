2a1b5260d4b06deb92b5d5fd7d5165eaf5e28184
jiangxd <806089486@qq.com>
2020-05-29 16:00:49 +0800

    登录model

--- ruoyi-service/ruoyi-auth/src/main/java/com/ruoyi/auth/form/LoginForm.java
+++ ruoyi-service/ruoyi-auth/src/main/java/com/ruoyi/auth/form/LoginForm.java
@@ -1,11 +1,17 @@
 package com.ruoyi.auth.form;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-public class LoginForm
-{
+@ApiModel
+public class LoginForm {
+
+    @ApiModelProperty(value = "用户名", example = "admin", required = true)
     private String username;
 
+    @ApiModelProperty(value = "密码", example = "admin", required = true)
     private String password;
+    
 }
