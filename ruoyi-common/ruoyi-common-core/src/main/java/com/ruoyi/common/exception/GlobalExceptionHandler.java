66fd88bfb076181cf2c2b54a123cfcc078e8bef0
jiangxd <806089486@qq.com>
2020-06-01 16:16:26 +0800

    quan全局

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/exception/GlobalExceptionHandler.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.ruoyi.common.exception;
 
 import com.ruoyi.common.core.domain.RE;
-import com.ruoyi.common.core.domain.Rest;
-import com.ruoyi.common.utils.RestUtil;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -21,46 +18,49 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @author lucas
  */
 @RestControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * 请求方式不支持
      */
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
     @ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
-    public Rest handleException(HttpRequestMethodNotSupportedException e) {
-        log.error("HttpRequestMethodNotSupportedException异常");
-        return RestUtil.error(500,"不支持' " + e.getMethod() + "'请求");
+    public RE handleException(HttpRequestMethodNotSupportedException e) {
+        logger.error(e.getMessage(), e);
+        return RE.error(500, "不支持' " + e.getMethod() + "'请求");
     }
 
     /**
      * 拦截未知的运行时异常
      */
     @ExceptionHandler(RuntimeException.class)
-    public Rest notFount(RuntimeException e) {
-        log.error("RuntimeException异常");
-        return RestUtil.error(500,"运行时异常:" + e.getMessage());
+    public RE notFount(RuntimeException e) {
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+        logger.error("运行时异常:", e);
+        return RE.error(500, "运行时异常:" + e.getMessage());
     }
 
     /**
      * 处理自定义异常
      */
     @ExceptionHandler(RuoyiException.class)
-    public Rest handleWindException(RuoyiException e) {
-        return RestUtil.error(e.getCode(), e.getMessage());
+    public RE handleWindException(RuoyiException e) {
+        return RE.error(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
-    public Rest handleDuplicateKeyException(DuplicateKeyException e) {
-        log.error(e.getMessage(), e);
-        return RestUtil.error(500,"数据库中已存在该记录");
+    public RE handleDuplicateKeyException(DuplicateKeyException e) {
+        logger.error(e.getMessage(), e);
+        return RE.error("数据库中已存在该记录");
     }
 
     @ExceptionHandler(Exception.class)
-    public Rest handleException(Exception e) throws Exception {
-        log.error(e.getMessage(), e);
-        return RestUtil.error(500, "参数或服务器异常，请联系管理员");
+    public RE handleException(Exception e) throws Exception {
+        logger.error(e.getMessage(), e);
+        return RE.error(500, "服务器错误，请联系管理员");
     }
 
     /**
@@ -70,13 +70,13 @@ public class GlobalExceptionHandler {
      * @return 统一封装的结果类, 含有代码code和提示信息msg
      */
     @ExceptionHandler(UnauthorizedException.class)
-    public Rest handle401(UnauthorizedException e) {
-        return RestUtil.error(401, e.getMessage());
+    public RE handle401(UnauthorizedException e) {
+        return RE.error(401, e.getMessage());
     }
 
     // 验证码错误
     @ExceptionHandler(ValidateCodeException.class)
-    public Rest handleCaptcha(ValidateCodeException e) {
-        return RestUtil.error(500,e.getMessage());
+    public RE handleCaptcha(ValidateCodeException e) {
+        return RE.error(e.getMessage());
     }
 }
