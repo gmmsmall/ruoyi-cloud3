0d235378fdfdef727cacd26d1046f27c7d253920
GuoMiaomiao <863277470@qq.com>
2020-06-02 10:30:27 +0800

    分组管理的新增修改判重，修改参数改为实体类请求

--- ruoyi-service/ruoyi-javamail/src/main/java/com/ruoyi/javamail/entity/SendMailGroupItems.java
+++ ruoyi-service/ruoyi-javamail/src/main/java/com/ruoyi/javamail/entity/SendMailGroupItems.java
@@ -2,40 +2,51 @@ package com.ruoyi.javamail.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 /**
- * 
- *
+ * 分组管理子表
  * @author gmm
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@ApiModel(value = "com.ruoyi.javamail.entity.SendMailGroupItems",description = "分组管理子表")
 public class SendMailGroupItems implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value = "主键id",hidden = true)
     private Long id;//主键
 
     /**
      * 分组主键
      */
+    @ApiModelProperty("分组主键id，必填")
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long fid;
 
     /**
      * 院士编码
      */
+    @ApiModelProperty("院士编码，必填")
+    @NotEmpty
     private String acadencode;
 
     /**
      * 删除标记 1=未删除  2= 删除
      */
+    @ApiModelProperty(value = "删除标记 1=未删除  2= 删除",hidden = true)
     private String deleteflag;
 
 }
--- ruoyi-service/ruoyi-javamail/src/main/java/com/ruoyi/javamail/service/impl/SendMailGroupServiceImpl.java
+++ ruoyi-service/ruoyi-javamail/src/main/java/com/ruoyi/javamail/service/impl/SendMailGroupServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ruoyi.javamail.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.javamail.bo.SendMailGroupBo;
+import com.ruoyi.javamail.bo.SendMailGroupEditBo;
 import com.ruoyi.javamail.dao.SendMailGroupMapper;
 import com.ruoyi.javamail.entity.SendMailGroup;
 import com.ruoyi.javamail.entity.SendMailGroupItems;
@@ -12,6 +14,7 @@ import com.ruoyi.javamail.util.FebsUtil;
 import com.ruoyi.javamail.vo.SendMailGroupVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,36 +41,55 @@ public class SendMailGroupServiceImpl extends ServiceImpl<SendMailGroupMapper, S
     public void saveGroup(SendMailGroupBo sendMailGroupBo) {
         log.info("新增一个分组");
         try{
-            LocalDateTime now = LocalDateTime.now();
-            SendMailGroup group = new SendMailGroup();
-            group.setName(sendMailGroupBo.getName());
-            group.setName(sendMailGroupBo.getName());//分组名称
-            group.setAddperson(sendMailGroupBo.getAddperson());//新增人
-            group.setAddpersonid(sendMailGroupBo.getAddpersonid());//新增人id
-            group.setAddtime(now);
-            group.setEditperson(sendMailGroupBo.getAddperson());//修改人
-            group.setEditpersonid(sendMailGroupBo.getAddpersonid());//修改人id
-            group.setEdittime(now);
-            group.setDeleteflag("1");//未删除
-            this.baseMapper.insert(group);
+            //判断当前的分组名称是否已存在
+            SendMailGroup sendMailGroup = this.groupMapper.selectOne(new QueryWrapper<SendMailGroup>()
+                    .eq("deleteflag","1")
+                    .eq("addpersonid",sendMailGroupBo.getAddpersonid())
+                    .eq("name",sendMailGroupBo.getName()));
+            if(sendMailGroup != null){//分组名称已存在，不允许新增重复的
+                throw new DuplicateKeyException("数据已存在，操作失败");
+            }else{
+                LocalDateTime now = LocalDateTime.now();
+                SendMailGroup group = new SendMailGroup();
+                group.setName(sendMailGroupBo.getName());
+                group.setName(sendMailGroupBo.getName());//分组名称
+                group.setAddperson(sendMailGroupBo.getAddperson());//新增人
+                group.setAddpersonid(sendMailGroupBo.getAddpersonid());//新增人id
+                group.setAddtime(now);
+                group.setEditperson(sendMailGroupBo.getAddperson());//修改人
+                group.setEditpersonid(sendMailGroupBo.getAddpersonid());//修改人id
+                group.setEdittime(now);
+                group.setDeleteflag("1");//未删除
+                this.baseMapper.insert(group);
+            }
         }catch (Exception e){
             log.error("新增分组失败");
             throw e;
         }
     }
 
+    /**
+     * 修改分组的具体操作
+     * @param sendMailGroup
+     * @param itemsList
+     */
     @Override
     @Transactional
     public void updateGroupById(SendMailGroup sendMailGroup,List<SendMailGroupItems> itemsList) {
-        if(sendMailGroup != null){
-            if(itemsList != null && itemsList.size() > 0){
-                //分组子表进行先删后增操作
-                LambdaQueryWrapper<SendMailGroupItems> queryWrapper = new LambdaQueryWrapper<SendMailGroupItems>();
-                queryWrapper.eq(SendMailGroupItems::getFid,sendMailGroup.getId());
-                itemsService.remove(queryWrapper);//先删除
-                itemsService.saveBatch(itemsList);//后增加
+        try{
+            if(sendMailGroup != null){
+                if(itemsList != null && itemsList.size() > 0){
+                    //分组子表进行先删后增操作
+                    LambdaQueryWrapper<SendMailGroupItems> queryWrapper = new LambdaQueryWrapper<SendMailGroupItems>();
+                    queryWrapper.eq(SendMailGroupItems::getFid,sendMailGroup.getId());
+                    this.itemsService.remove(queryWrapper);//先删除
+                    this.itemsService.saveBatch(itemsList);//后增加
+                }
+                this.baseMapper.updateById(sendMailGroup);
             }
-            this.baseMapper.updateById(sendMailGroup);
+        }catch (Exception e){
+            log.error("修改分组失败");
+            throw e;
         }
 
     }
@@ -106,5 +128,39 @@ public class SendMailGroupServiceImpl extends ServiceImpl<SendMailGroupMapper, S
     @Override
     public List<SendMailGroupVo> groupList(Long userId) {
         return this.groupMapper.groupList(userId);
+    }
+
+    /**
+     * 修改分组（主子表同时修改）
+     * @param sendMailGroupEditBo
+     */
+    @Override
+    @Transactional
+    public void updateGroupByEntity(SendMailGroupEditBo sendMailGroupEditBo) {
+        log.info("修改分组");
+        try{
+            //判断当前的分组名称是否已存在
+            SendMailGroup sendGroup = this.groupMapper.selectOne(new QueryWrapper<SendMailGroup>()
+                    .eq("deleteflag","1")
+                    .eq("addpersonid",sendMailGroupEditBo.getEditpersonid())
+                    .eq("name",sendMailGroupEditBo.getName())
+                    .notIn("id",sendMailGroupEditBo.getId()));
+            if(sendGroup != null){//分组名称已存在，不允许重复的
+                throw new DuplicateKeyException("数据已存在，操作失败");
+            }else{
+                SendMailGroup sendMailGroup = new SendMailGroup();
+                //更新的分组主表中的内容
+                sendMailGroup.setId(sendMailGroupEditBo.getId());
+                sendMailGroup.setName(sendMailGroupEditBo.getName());
+                sendMailGroup.setEditpersonid(sendMailGroupEditBo.getEditpersonid());
+                sendMailGroup.setEditperson(sendMailGroupEditBo.getEditperson());
+                LocalDateTime now = LocalDateTime.now();
+                sendMailGroup.setEdittime(now);
+                this.updateGroupById(sendMailGroup,sendMailGroupEditBo.getItemsList());
+            }
+        }catch (Exception e){
+            log.error("修改分组失败");
+            throw e;
+        }
     }
 }
