b3e2933fae324d0fca7a1060cc8727ed0f53e6b2
GuoMiaomiao <863277470@qq.com>
2020-06-02 18:04:17 +0800

    院士论文管理swagger

--- ruoyi-service/ruoyi-acad/src/main/java/com/ruoyi/acad/controller/PaperController.java
+++ ruoyi-service/ruoyi-acad/src/main/java/com/ruoyi/acad/controller/PaperController.java
@@ -3,9 +3,12 @@ package com.ruoyi.acad.controller;
 import com.ruoyi.acad.domain.Paper;
 import com.ruoyi.acad.domain.ResponseResult;
 import com.ruoyi.acad.service.IPaperService;
+import com.ruoyi.common.core.domain.RE;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 
@@ -16,6 +19,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/paper")
+@Api(tags = "论文信息管理")
 public class PaperController {
 
 	@Autowired
@@ -29,24 +33,28 @@ public class PaperController {
 	 * @throws Exception
 	 */
 	@PostMapping("/saveModel")
-	public ResponseResult saveModel(@RequestBody List<Paper> paperList, @NotBlank Integer acadId) throws Exception {
-		
-		paperService.saveModel(paperList, acadId);
-		return new ResponseResult(true, 200, "保存成功");
+	@ApiOperation(value = "新增论文信息")
+	@ApiResponses({@ApiResponse(code = 200,message = "保存成功")})
+	public RE saveModel(@Valid @RequestBody@ApiParam(value = "院士论文列表",required = true) List<Paper> paperList,
+						@ApiParam(value = "院士id",required = true)@RequestParam Integer acadId) throws Exception {
+		this.paperService.saveModel(paperList, acadId);
+		return new RE().ok("保存成功");
 	}
 
 	/**
-	 * Description:保存论文信息
+	 * Description:修改论文信息
 	 * CreateTime:2020年3月31日上午11:08:02
 	 * @param paperList
 	 * @return
 	 * @throws Exception
 	 */
 	@PostMapping("/updateModel")
-	public ResponseResult updateModel(@RequestBody List<Paper> paperList, @NotBlank Integer acadId) throws Exception {
-
-		paperService.updateModel(paperList, acadId);
-		return new ResponseResult(true, 200, "修改成功");
+	@ApiOperation(value = "修改论文信息")
+	@ApiResponses({@ApiResponse(code = 200,message = "修改成功")})
+	public RE updateModel(@Valid @RequestBody@ApiParam(value = "院士论文列表",required = true) List<Paper> paperList,
+						  @ApiParam(value = "院士id",required = true)@RequestParam Integer acadId) throws Exception {
+		this.paperService.updateModel(paperList, acadId);
+		return new RE().ok("修改成功");
 	}
 	
 	/**
@@ -57,9 +65,9 @@ public class PaperController {
 	 * @throws Exception
 	 */
 	@GetMapping("/getModelById")
-	public List<Paper> getModelById(Integer acadId) throws Exception {
-
-		List<Paper> paperList = paperService.getModelByAcadId(acadId);
-		return paperList;
+	@ApiOperation(value = "根据院士ID获取院士论文信息")
+	@ApiResponses({@ApiResponse(code = 200,message = "查询成功")})
+	public List<Paper> getModelById(@ApiParam(value = "院士id",required = true)@RequestParam Integer acadId) throws Exception {
+		return this.paperService.getModelByAcadId(acadId);
 	}
 }
--- ruoyi-service/ruoyi-acad/src/main/java/com/ruoyi/acad/domain/Paper.java
+++ ruoyi-service/ruoyi-acad/src/main/java/com/ruoyi/acad/domain/Paper.java
@@ -1,10 +1,14 @@
 package com.ruoyi.acad.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -21,24 +25,33 @@ public class Paper implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty("院士编码")
+    @JsonSerialize(using = ToStringSerializer.class)
+    @NotNull
     private Integer acadId;
 
+    @ApiModelProperty("论文ID")
     private String paperId;//论文ID
 
+    @ApiModelProperty("论文题目")
     private String paperTitle;//论文题目
 
+    @ApiModelProperty("论文摘要")
     private String paperAbstract;//论文摘要
 
+    @ApiModelProperty("发表时间")
     private LocalDate publishedTime;//发表时间
+
+    @ApiModelProperty("引用数量")
+    private Integer hIndex;//引用数量
 
     /**
      * 1-SCI，2-EI，3-IEEE，4-其他
      */
+    @ApiModelProperty("论文类别 1-SCI，2-EI，3-IEEE，4-其他")
     private Integer periodical;//论文类别
 
+    @ApiModelProperty("网页链接")
     private String paperWebsiteLink;//网页链接
-
-    private Integer hIndex;//引用数量
-
 
 }
