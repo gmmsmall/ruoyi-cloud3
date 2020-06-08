09d5fb1a45dfabd8b7b39f9e98f7452d41a26fd2
jiangxd <806089486@qq.com>
2020-06-08 09:50:57 +0800

    日志

--- ruoyi-service/ruoyi-mb-system/src/main/java/com/ruoyi/system/controller/AcadOperLogController.java
+++ ruoyi-service/ruoyi-mb-system/src/main/java/com/ruoyi/system/controller/AcadOperLogController.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("acadLog")
-@Api(value = "acadLog", description = "操作日志")
+@Api(value = "acadLog", description = "操作日志--院士信息日志")
 public class AcadOperLogController extends BaseController {
 
     @Autowired
