0cfb4b25dd59c34aa99d686512a2f71a2dc313b3
jiangxd <806089486@qq.com>
2020-06-10 09:41:44 +0800

    gateway redis 集群

---
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/domain/RedisClusterConfigProperties.java
@@ -0,0 +1,18 @@
+package com.ruoyi.gateway.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+@Data //使用了lombok的标签 如果未引用lombok需写getter 和 setter方法
+public class RedisClusterConfigProperties {
+    private List<String> nodes;
+    private Integer maxAttempts;
+    private Integer connectionTimeout;
+    private Integer soTimeout;
+    private String password;
+}
